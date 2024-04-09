abd97511a673ad28c84439d66ab649d289faf2c0
Tyler Howell <tyler.howell1202@gmail.com>
2024-04-08 21:08:31 -0500

    Added regex to invoice_parser.rb

--- invoice_parser.rb
+++ invoice_parser.rb
@@ -1,7 +1,7 @@
 def parse(x)
     invoices = []
     x.each_line do |i|
-        string_match = i.match()
+        string_match = i.match(/(\d{2}\/\d{2}\/\d{4}) - (\d+) - ([\w\s]+) - (\d+\.\d{2})/)
         if string_match
             date = string_match[1]
             invoice_num = string_match[2]
@@ -12,3 +12,15 @@ def parse(x)
     end
     invoices
 end
+
+if ARGV.empty?
+    puts "Help: "
+    puts "ruby invoice_parser.rb <file.txt>"
+else
+    user_input = ARGV[0]
+    text = File.read(user_input)
+    invoices = parse(text)
+
+    invoices.each do |j|
+        puts "Date: #{j[:date]}, Number: #{j[:invoice_num]}, Name: #{j[:client_name]}, Sum: $#{j[:amount]}""
+    end
